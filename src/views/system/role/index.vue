587657c8787cb8f976d8610d8230b48f2839aa6b
codesensi <codesensi@aliyun.com>
2026-09-06 19:49:17 +0800

    fix(role): 修复角色状态选项值错误
    - 纠正了启用状态的选项值从 1 改为 0
    - 纠正了停用状态的选项值从 0 改为 1
    - 修复了下拉选择框中的状态值映射关系

--- src/views/system/role/index.vue
+++ src/views/system/role/index.vue
@@ -117,8 +117,8 @@ onMounted(() => {
           clearable
           class="w-45!"
         >
-          <el-option label="已启用" value="1" />
-          <el-option label="已停用" value="0" />
+          <el-option label="已启用" value="0" />
+          <el-option label="已停用" value="1" />
         </el-select>
       </el-form-item>
       <el-form-item>
